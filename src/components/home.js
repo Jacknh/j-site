0bfbc30f2e4f602b0b504007b43e76532062fcac
jack <jacknh418@gmail.com>
2020-05-08 14:19:23 +0800

    blur background image on home page

--- src/components/home.js
+++ src/components/home.js
@@ -4,17 +4,21 @@ import styles from "./home.module.css"
 
 const Home = () => (
   <div className={styles.wrapper}>
-    <Link to="/tech" className={`${styles.item} ${styles.tech}`}>
-      <div>Tech</div>
+    <Link to="/tech" className={`${styles.item}`}>
+      <div className={styles.tech}></div>
+      <div className={styles.wording}>Tech</div>
     </Link>
-    <Link to="/life" className={`${styles.item} ${styles.life}`}>
-      <div>Life</div>
+    <Link to="/life" className={`${styles.item}`}>
+      <div className={styles.life}></div>
+      <div className={styles.wording}>Life</div>
     </Link>
-    <Link to="/music" className={`${styles.item} ${styles.music}`}>
-      <div>Music</div>
+    <Link to="/music" className={`${styles.item}`}>
+      <div className={styles.music}></div>
+      <div className={styles.wording}>Music</div>
     </Link>
-    <Link to="/thinking" className={`${styles.item} ${styles.thinking}`}>
-      <div>Thinking</div>
+    <Link to="/thinking" className={`${styles.item}`}>
+      <div className={styles.thinking}></div>
+      <div className={styles.wording}>Thinking</div>
     </Link>
   </div>
 )
