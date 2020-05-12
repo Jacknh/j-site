07cc59c0477426054965d4271814c7e7bf9c4d49
jack <jacknh418@gmail.com>
2020-05-12 19:49:49 +0800

    style changes

--- src/components/home.js
+++ src/components/home.js
@@ -4,20 +4,16 @@ import styles from "./home.module.css"
 
 const Home = () => (
   <div className={styles.wrapper}>
-    <Link to="/tech" className={`${styles.item}`}>
-      <div className={styles.tech}></div>
+    <Link to="/tech" className={`${styles.item} ${styles.tech}`}>
       <div className={styles.wording}>Tech</div>
     </Link>
-    <Link to="/life" className={`${styles.item}`}>
-      <div className={styles.life}></div>
+    <Link to="/life" className={`${styles.item} ${styles.life}`}>
       <div className={styles.wording}>Life</div>
     </Link>
-    <Link to="/music" className={`${styles.item}`}>
-      <div className={styles.music}></div>
+    <Link to="/music" className={`${styles.item} ${styles.music}`}>
       <div className={styles.wording}>Music</div>
     </Link>
-    <Link to="/thinking" className={`${styles.item}`}>
-      <div className={styles.thinking}></div>
+    <Link to="/thinking" className={`${styles.item} ${styles.thinking}`}>
       <div className={styles.wording}>Thinking</div>
     </Link>
   </div>
